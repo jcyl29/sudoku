5480402c34971ad4b0dfa56da5d0dea22e96af5b
Justin Lui <jlui@surveymonkey.com>
2015-03-24 23:33:13 -0700

    now distinguishing for odd subgrids

--- grid.js
+++ grid.js
@@ -9,12 +9,13 @@ var totalSubdivisions = Math.sqrt(totalSubgrids);
 for (row = 1; row <= totalSubgrids; row++) {
     html += "<tr>";
     for (col = 1; col <= totalSubgrids; col++) {
-        sectRow = Math.ceil( row / totalSubdivisions );
-        sectCol = Math.ceil( col / totalSubdivisions );
+        sectRow = Math.ceil(row / totalSubdivisions);
+        sectCol = Math.ceil(col / totalSubdivisions);
         sectId = sectRow + "" + sectCol;
+        oddClassName = ((sectCol + sectRow) % 2 !== 0) ? "odd" : "";
 
-        html += "<td data-subgrid='" + sectId + "' data-col=" + col + " data-row=" + row + ">" +
-            "<input maxlength=10 size=25 type='text' value='" + row + col + " sectId=" + sectId + "'></td>";
+        html += "<td data-subgrid='" + sectId + "' data-col=" + col + " data-row=" + row + " class='" + oddClassName + "'>" +
+            "<input maxlength=10 size=10 type='text' value='" + row + col + "isOdd=" + oddClassName + "'></td>";
     }
     html += "</tr>";
 
